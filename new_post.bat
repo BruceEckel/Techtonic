4a9516f9cde6463bcb3ed8de792a13aceb82649a
Bruce Eckel <mindviewinc@gmail.com>
2024-08-29 22:56:04 -0600

    Update new_post.bat

--- new_post.bat
+++ new_post.bat
@@ -1,9 +1,3 @@
 @echo off
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0_post.ps1"
-pause
-if exist "_posts\" (
-    for /r "_posts\" %%f in (*.md) do (
-        set "newest_file=%%f"
-    )
-    code "%newest_file%"
-)
+@REM pause
